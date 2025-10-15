d8fecb8765454e3d6f22e1f6dbc4fea1aa135a73
Charcrede <charcrede@gmail.com>
2025-10-15 21:43:31 +0200

    retrait de limite sur title et head

--- src/modules/articles/entities/article.entity.ts
+++ src/modules/articles/entities/article.entity.ts
@@ -1,34 +1,34 @@
-import { Entity, Column, ManyToOne, OneToMany, JoinColumn } from 'typeorm';
-import { BaseEntity } from '@/common/entities/base.entity';
-import { Rubric } from '@/modules/rubrics/entities/rubric.entity';
-import { Admin } from '@/modules/admins/entities/admin.entity';
-import { Media } from '@/modules/media/entities/media.entity';
+  import { Entity, Column, ManyToOne, OneToMany, JoinColumn } from 'typeorm';
+  import { BaseEntity } from '@/common/entities/base.entity';
+  import { Rubric } from '@/modules/rubrics/entities/rubric.entity';
+  import { Admin } from '@/modules/admins/entities/admin.entity';
+  import { Media } from '@/modules/media/entities/media.entity';
 
-@Entity('articles')
-export class Article extends BaseEntity {
-  @Column({ length: 255 })
-  title: string;
+  @Entity('articles')
+  export class Article extends BaseEntity {
+    @Column('text')
+    title: string;
 
-  @Column('text')
-  content: string;
+    @Column('text')
+    content: string;
 
-  @Column('uuid')
-  rubric_id: string;
+    @Column('uuid')
+    rubric_id: string;
 
-  @Column({ length: 255 })
-  head: string;
+    @Column('text')
+    head: string;
 
-  @Column('uuid')
-  created_by: string;
+    @Column('uuid')
+    created_by: string;
 
-  @ManyToOne(() => Rubric, (rubric) => rubric.articles)
-  @JoinColumn({ name: 'rubric_id' })
-  rubric: Rubric;
+    @ManyToOne(() => Rubric, (rubric) => rubric.articles)
+    @JoinColumn({ name: 'rubric_id' })
+    rubric: Rubric;
 
-  @ManyToOne(() => Admin, (admin) => admin.articles)
-  @JoinColumn({ name: 'created_by' })
-  creator: Admin;
+    @ManyToOne(() => Admin, (admin) => admin.articles)
+    @JoinColumn({ name: 'created_by' })
+    creator: Admin;
 
-  @OneToMany(() => Media, (media) => media.article)
-  media: Media[];
-}
+    @OneToMany(() => Media, (media) => media.article)
+    media: Media[];
+  }
